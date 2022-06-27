8469fe7bdeddaaf6376d1eb0b7c4d1f9ab5c15ae
Younis <younisrahman007@gmail.com>
2022-06-28 00:04:14 +0600

    complete runtime err

--- ArrayManipulation.py
+++ ArrayManipulation.py
@@ -23,17 +23,30 @@ def arrayManipulation(n, queries):
     #     for j in range(i[0], i[1]+1):
     #         arr[j] += i[2]
     # return max(arr)
-    arr = [0] * (n + 1)
-    for a, b, k in queries:
-        arr[a-1] += k
-        arr[b] -= k
-    max_val = temp = 0
-    for i in arr:
-        temp += i
-        print(temp)
-        if temp > max_val:
-            max_val = temp
-    return max_val
+    # arr = [0] * (n + 1)
+    # for a, b, k in queries:
+    #     arr[a-1] += k
+    #     arr[b] -= k
+    # max_val = temp = 0
+    # for i in arr:
+    #     temp += i
+    #     print(temp)
+    #     if temp > max_val:
+    #         max_val = temp
+    # return max_val
+    # arr = [0]*(n+2)
+
+    # # perform the query operations
+    # for a, b, k in queries:
+    #     arr[a] += k
+    #     arr[b+1] -= k
+    #     print(arr)
+    # # find max element from the array
+    # maxnum = temp = 0
+    # for val in arr:
+    #     temp += val
+    #     maxnum = max(maxnum, temp)
+    # return maxnum
 
 
 first_multiple_input = input().rstrip().split()
