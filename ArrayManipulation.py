8158bcdf26ad51fda4d1cf234c73837fcdd7eb56
Younis <younisrahman007@gmail.com>
2022-06-27 15:10:26 +0600

    Create Array

--- ArrayManipulation.py
+++ ArrayManipulation.py
@@ -1,1 +1,39 @@
-print('Hi')
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    arr = [[0] * (n)] * len(queries)
+    for i in range(len(queries)):
+    return arr
+
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+queries = []
+
+for _ in range(m):
+    queries.append(list(map(int, input().rstrip().split())))
+
+result = arrayManipulation(n, queries)
+
+print(result)
