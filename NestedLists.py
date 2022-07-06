4fd2c306970ff260dd62fa00c9716e9aeec0ef14
Younis <younisrahman007@gmail.com>
2022-07-06 16:36:18 +0600

    Nested list

--- NestedLists.py
+++ NestedLists.py
@@ -1,14 +1,44 @@
-names = {}
+names = set()
 minimun = 0
+lowest = 0
 for i in range(int(input())):
 
     name = input()
     score = float(input())
     if i == 0:
+        lowest = score
         minimun = score
-    if score < minimun:
-        minimun = score
+    if score < lowest:
+        lowest = score
+
+    if score > lowest and score < minimun:
         names = {name}
-    elif minimun == score:
+        minimun = score
+    elif score == minimun:
         names.add(name)
-print(names)
+
+names = sorted(names)
+for i in names:
+    print(i)
+
+# 4
+# Prashant
+# 32
+# Pallavi
+# 36
+# Dheeraj
+# 39
+# Shivam
+# 40
+
+# 5
+# Harry
+# 37.21
+# Berry
+# 37.21
+# Tina
+# 37.2
+# Akriti
+# 41
+# Harsh
+# 39
