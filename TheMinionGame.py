b4494f78e6334f32657e7a35bbbb6bcaa2889526
Younis <younisrahman007@gmail.com>
2022-07-10 00:24:19 +0600

    The Minion Game

---
+++ TheMinionGame.py
@@ -0,0 +1,21 @@
+def minion_game(string):
+    # your code goes here
+    stuart = 0
+    kevin = 0
+    count = len(string)
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    string = string.lower()
+    string = string.replace(' ', '')
+    for i in string:
+        if i in vowels:
+            kevin += count
+        else:
+            stuart += count
+        count -= 1
+    print('Kevin {}'.format(kevin) if kevin >
+          stuart else 'Stuart {}'.format(stuart))
+
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
