0466688e40fb61404c258a2c46b72977e861109d
Younis <younisrahman007@gmail.com>
2022-08-03 12:48:03 +0600

    Find the point

---
+++ FindThePoint.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'findPoint' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER px
+ *  2. INTEGER py
+ *  3. INTEGER qx
+ *  4. INTEGER qy
+ */
+
+vector<int> findPoint(int px, int py, int qx, int qy)
+{
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    for (int n_itr = 0; n_itr < n; n_itr++)
+    {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int px = stoi(first_multiple_input[0]);
+
+        int py = stoi(first_multiple_input[1]);
+
+        int qx = stoi(first_multiple_input[2]);
+
+        int qy = stoi(first_multiple_input[3]);
+
+        vector<int> result = findPoint(px, py, qx, qy);
+
+        for (size_t i = 0; i < result.size(); i++)
+        {
+            fout << result[i];
+
+            if (i != result.size() - 1)
+            {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
+
+vector<string> split(const string &str)
+{
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos)
+    {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
