75c0634300174941c31a7f51d623311bc45af200
Younis <younisrahman007@gmail.com>
2022-08-09 23:24:14 +0600

    LeonardoPrimeFactors

---
+++ LeonardoPrimeFactors.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+int primeFactors(long n)
+{
+    int primes[] = {2,
+                    3,
+                    5,
+                    7,
+                    11,
+                    13,
+                    17,
+                    19,
+                    23,
+                    29,
+                    31,
+                    37,
+                    41,
+                    43,
+                    47,
+                    53,
+                    59,
+                    61,
+                    67,
+                    71,
+                    73,
+                    79,
+                    83,
+                    89,
+                    97};
+    int len = sizeof(primes) / sizeof(primes[0]);
+    long long ans = 1;
+    int count = 0;
+
+    if (n == 1)
+        return 0;
+    if (n == 1 || n == 2 || n == 3)
+        return 1;
+
+    for (int i = 0; i < len; i++)
+    {
+        std::cout << ans << std::endl;
+        ans *= primes[i];
+        if (ans <= n)
+        {
+            count++;
+        }
+        else
+            break;
+    }
+    return count;
+}
+int main()
+{
+    int n, b, c, d;
+    vector<int> vector;
+    set<int> set;
+    cin >> n;
+    while (n--)
+    {
+        long a;
+        cin >> a;
+        cout << primeFactors(a) << endl;
+    }
+    return 0;
+}
