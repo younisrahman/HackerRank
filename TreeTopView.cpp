5eda30852caef67e1054ec5570ecd7137f4e6f5d
Younis Rahman <younisrahman007@gmail.com>
2022-10-13 15:53:46 +0600

    top

---
+++ TreeTopView.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void topView(Node *root)
+{
+}
+
+int main()
+{
+
+    FastIO;
+    // 1 2 5 3 4 6
+    Node *h1 = new Node(1);
+    h1->right = new Node(2);
+    h1->right->right = new Node(5);
+    h1->right->right->right = new Node(6);
+    h1->right->right->left = new Node(3);
+    h1->right->right->left->left = new Node(4);
+
+    topView(h1);
+
+    return 0;
+}
